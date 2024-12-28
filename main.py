4133c52cc08d8b841163292686c9ade69c33ff3b
Shricharan K.S <62522322+shricharanks108@users.noreply.github.com>
2024-12-27 22:22:03 -0500

    Run 100 epochs with GPU

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import ultralytics
 from ultralytics import YOLO
+import torch
 
-model = YOLO("yolov8n.pt")
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'Using device: {device}')
+    model = YOLO("yolov8n.pt").to(device)
 
-# def on_train_epoch_end(trainer):
-#     result = model("potholes/output/train/images/potholes0.png")
-#     result.save(save_dir = "potholes/example")
-#
-# model.add_callback("on_train_epoch_end", on_train_epoch_end)
+    # def on_train_epoch_end(trainer):
+    #     result = model("potholes/output/train/images/potholes0.png")
+    #     result.save(save_dir = "potholes/example")
+    #
+    # model.add_callback("on_train_epoch_end", on_train_epoch_end)
 
-train_results = model.train(data="data.yaml", epochs=50, imgsz = 640)
+    train_results = model.train(data="data.yaml", epochs=100, imgsz = 640)
 
-model.export()
+    model.export()
